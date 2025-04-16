9b39c5c8d1517865431b100cd432594552f3c14f
Adi Susilo <mr112@mr112.mr112.ney>
2025-04-16 09:56:46 +0700

    secondary commit

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,46 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Sekolah</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.js"></script>
 </head>
+
 <body>
-    <header>
-        <h1>Informasi Sekolah</h1>
+
+    <!---Pre Loader--->
+    <div class="preloader">
+        <div class="loading">
+            <img src="../assets/img/loading.gif" width="80">
+        </div>
+    </div>
+
+    <!---Navbar--->
+    <header class="top-bar">
+        <a href="#" id="logo" name="logo"></a>
+        <div class="container">
+            <h1>Informasi Sekolah</h1>
+            <p>Jl.XXXX No.XXX Kecamatan XXX, Kota XXX, Jawa Tengah</p>
+            <p>Telp: (021) 12345678</p>
+        </div>
     </header>
-    <nav>
-        <ul>
-            <li><a href="index.php">Beranda</a></li>
-            <li><a href="pages/tentang.php">Tentang Kami</a></li>
-            <li><a href="pages/kontak.php">Kontak</a></li>
-            <li><a href="pages/berita.php">Berita</a></li>
-        </ul>
-    </nav>
+    <header>
+        <div class="header-wrapper">
+            <div class="header-title">
+                <nav class="header-title">
+                    <a href="#tentang" link="pages/tentang.php">Tentang</a>
+                    <a href="#berita" link="pages/berita.php">Berita</a>
+                    <a href="#kontak" link="pages/kontak.php">Kontak</a>
+                </nav>
+            </div>
+    </header>
